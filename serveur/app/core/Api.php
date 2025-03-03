c7045a6b84c0670f36d79eb0dff1408fa0805562
Thomas <thom576xd@gmail.com>
2025-03-03 17:04:48 +0100

    🚧(productDisplay): implements products generation for display

--- serveur/app/core/Api.php
+++ serveur/app/core/Api.php
@@ -1,9 +1,11 @@
 <?php
 require_once('./app/controllers/ExempleController.php');
 require_once('./app/controllers/AuthController.php');
+require_once('./app/controllers/ProductController.php');
 
 function setupRoutes($router) {
     $router->add('GET', "/api/home", [ExempleController::class, 'index']);
     $router->add('POST', "/api/auth/login", [AuthController::class, 'login']);
     $router->add('POST', "/api/auth/register", [AuthController::class, 'register']);
+    $router->add('GET', "/api/getAllProducts", [ProductController::class, "getAllProducts"]);
 }
---
+++ serveur/app/services/ProductService.php
@@ -0,0 +1,14 @@
+<?php
+
+class ProductService {
+    public static function getAllProducts() {
+        // TO DO: Implement products logic
+        try {
+            $products = [];
+            return $products;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+}
