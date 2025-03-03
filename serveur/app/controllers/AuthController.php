d11d74ab70cf0024f6dc8f927fb296ff4b87084c
invader237 <diego.trivino.fr@gmail.com>
2025-03-03 23:31:28 +0100

    :sparkles:(auth): implement authentication logic

--- serveur/app/controllers/AuthController.php
+++ serveur/app/controllers/AuthController.php
@@ -5,7 +5,6 @@ class AuthController {
     public static function login() {
         header('Content-Type: application/json');
 
-<<<<<<< HEAD
         $body = file_get_contents('php://input');
         $data = json_decode($body, true); 
 
@@ -19,18 +18,6 @@ class AuthController {
         }
 
         $jwt = AuthService::login($email, $password);
-=======
-        $id = $_POST['id'] ?? null;
-        $password = $_POST['password'] ?? null;
-
-        if (!$id || !$password) {
-            http_response_code(400);
-            echo json_encode(["message" => "ID et mot de passe requis"]);
-            return;
-        }
-
-        $jwt = AuthService::login($id, $password);
->>>>>>> a8b2ccf (:sparkles:(validation): check required parameters and return error if missing)
 
         if ($jwt) {
             echo json_encode(["token" => $jwt]);
