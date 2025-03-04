bf5997182aca88aabbed963eb62b7f2fdaf1961e
invader237 <diego.trivino.fr@gmail.com>
2025-03-05 00:10:01 +0100

    :construction:(Auth): Start implementing registration feature

--- serveur/app/DAO/UserDAO.php
+++ serveur/app/DAO/UserDAO.php
@@ -19,6 +19,7 @@ class UserDAO {
         return new User($row['id_utilisateur'], $row['prenom'], $row['nom'], $row['date_naissance'], $row['email'], $row['mdp'], $row['id_civilite']);
     }
 
+<<<<<<< HEAD
     public function getUserByEmail(string $email): ?User {
         $stmt = $this->pdo->prepare('SELECT * FROM UTILISATEUR WHERE email = :email');
         $stmt->execute(['email' => $email]);
@@ -29,6 +30,8 @@ class UserDAO {
         return new User($row['id_utilisateur'], $row['prenom'], $row['nom'], $row['date_naissance'], $row['email'], $row['mdp'], $row['id_civilite']);
     }
 
+=======
+>>>>>>> 94ab9e9 (:construction:(Auth): Start implementing registration feature)
     public function createUser(User $user): void {
         $stmt = $this->pdo->prepare('INSERT INTO UTILISATEUR (prenom, nom, date_naissance, email, mdp, id_civilite) VALUES (:prenom, :nom, :date_naissance, :email, :mdp, :id_civilite)');
         $stmt->execute([
