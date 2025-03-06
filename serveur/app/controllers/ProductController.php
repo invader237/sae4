10d8e8200cfedbb7048afe1c252726699aebc33c
Thomas <thom576xd@gmail.com>
2025-03-06 08:58:51 +0100

    🐛(productDisplay): fix products gathering

--- serveur/app/controllers/ProductController.php
+++ serveur/app/controllers/ProductController.php
@@ -5,11 +5,12 @@ class ProductController {
     public static function getAllProducts() {
         header('Content-Type: application/json');
 
-        $products = ProductController::getAllProducts();
+        $products = ProductService::getAllProducts();
 
-        try {
-            echo json_encode(["data" => $products]);
-        } catch (\Throwable $th) {
+        if ($products) {
+            $productsArray = array_map(fn($product) => $product->toArray(), $products);
+            echo json_encode(["data" => $productsArray], JSON_UNESCAPED_UNICODE);
+        } else {
             http_response_code(500);
             echo json_encode(["message" => "Une erreur interne est survenue. Veuillez réessayer plus tard."]);
         }
