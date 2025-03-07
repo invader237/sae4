a3cfbe1bf667322c8a766e70d5b81945c4f98254
KERBER Alexandre <alexandre.kerber1@etu.univ-lorraine.fr>
2025-03-07 11:19:15 +0100

    :fire:(componentsLoader.js): delete componentsLoader

--- client/assets/js/componentsLoader.js
+++
@@ -1,6 +0,0 @@
-function footer(link,id){
-    fetch(link)
-    .then(response => response.text())
-    .then(data=>document.getElementById(id).innerHTML=data)
-    .catch(error=>console.error("Erreur lors de l'affichage du footer : ",error));
-}
