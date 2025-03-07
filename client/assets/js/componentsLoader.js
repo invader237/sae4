f367f10d4d5fdb4a3a81b57cf38ec2a5f19646f1
KERBER Alexandre <alexandre.kerber1@etu.univ-lorraine.fr>
2025-03-07 11:08:25 +0100

    :recycle:(refactor): footer display function refactored

--- client/assets/js/componentsLoader.js
+++ client/assets/js/componentsLoader.js
@@ -1,4 +1,6 @@
-fetch("../../components/footer.html")
-.then(response => response.text())
-.then(data=>document.getElementById("printfooter").innerHTML=data)
-.catch(error=>console.error("Erreur lors de l'affichage du footer : ",error));
+function footer(link,id){
+    fetch(link)
+    .then(response => response.text())
+    .then(data=>document.getElementById(id).innerHTML=data)
+    .catch(error=>console.error("Erreur lors de l'affichage du footer : ",error));
+}
