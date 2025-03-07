363dfa4a5da9b334b872c2f82acce5eba7e7eb69
EskYzK <alexkerber54190@gmail.com>
2025-03-07 18:20:41 +0100

    :construction: WIP

--- client/www/assets/js/router.js
+++ client/www/assets/js/router.js
@@ -3,6 +3,10 @@ const routes = {
     "/test": "pages/test.html",
 };
 
+if (!window.location.hash) {
+    window.location.hash = "#/";
+}
+
 const loadPage = async () => {
     const path = window.location.hash.replace("#", "") || "/";
     const page = routes[path] || routes["/"];
