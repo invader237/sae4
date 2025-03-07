5ed476dc8608f76677d5405af5e6d3844f37f624
invader237 <diego.trivino.fr@gmail.com>
2025-03-07 22:52:07 +0100

    :bug:(config): add CORS configuration for API access

--- serveur/index.php
+++ serveur/index.php
@@ -2,7 +2,17 @@
 require_once('./app/core/Router.php');
 require_once('./app/core/Api.php');
 
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin:*');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
 
 $router = new Router();  
 setupRoutes($router);
