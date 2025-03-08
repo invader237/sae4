bd84db766a568c9c9c3560f7023c8f1817c0c263
invader237 <diego.trivino.fr@gmail.com>
2025-03-08 16:32:54 +0100

    :construction:(auth): WIP - add user registration and authentication

---
+++ client/assets/js/register.js
@@ -0,0 +1,31 @@
+import { register } from './core/api/api.js';
+
+console.log("register.js");
+
+const formRegister = document.querySelector("#registerForm");
+
+formRegister.addEventListener("submit", (event) => {
+    event.preventDefault();
+    registerUser();
+});
+
+async function registerUser() {
+    const nom = document.getElementById("nom").value;
+    const prenom = document.getElementById("prenom").value;
+    const date_naissance = document.getElementById("date_naissance").value;
+    const email = document.getElementById("mel").value;
+    const mdp = document.getElementById("mdp").value;
+    const id_civilite = 1;
+    console.log(nom, prenom, date_naissance, email, mdp, id_civilite);
+    await register(nom, prenom, date_naissance, email, mdp, id_civilite).then((response) => {
+        if (response) {
+            alert("Vous êtes inscrit");
+            window.location.href = "#/login";
+        }
+        else {
+            document.getElementById("MessageErreur").innerText = response.data.message;
+        }
+    }).catch((error) => {
+        console.log("error");
+    });
+}
