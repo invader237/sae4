7cf000fd59a9e06326d0fa26f0dfb52661bd4919
invader237 <diego.trivino.fr@gmail.com>
2025-03-08 16:32:54 +0100

    :construction:(auth): WIP - add user registration and authentication

---
+++ client/assets/js/login.js
@@ -0,0 +1,32 @@
+import { login } from './core/api/api.js';
+
+console.log("login.js");
+
+const form = document.querySelector("#loginForm");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    authentifier();
+});
+
+
+async function authentifier() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
+    await login(email, password).then((response) => {
+        if (response.status === 200) {
+            localStorage.setItem("authToken", response.data.token);
+            alert("Vous êtes connecté");
+            window.location.href = "/";
+        }
+        else {
+            document.getElementById("error").innerText = response.data.message
+        }
+    }).catch((error) => {
+        console.error("error");
+        console.error(error);
+        document.getElementById("error").innerText = error.data.message;
+    });
+}
+
