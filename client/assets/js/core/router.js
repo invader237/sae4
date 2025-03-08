b1a45699389373d00c3053ba332b97a7c7382777
invader237 <diego.trivino.fr@gmail.com>
2025-03-08 15:02:12 +0100

    :bug:(router): fix script loading issue in router

--- client/assets/js/core/router.js
+++ client/assets/js/core/router.js
@@ -1,17 +1,26 @@
 const routes = {
     "/": "pages/accueil.html",
-    "/test": "pages/test.html",
+    "/login": "pages/login.html",
+    "/register": "pages/register.html",
 };
 
 const loadPage = async () => {
     const path = window.location.hash.replace("#", "") || "/";
     const page = routes[path] || routes["/"];
-    
+
     try {
         const response = await fetch(page);
         if (!response.ok) throw new Error("Page not found");
         const content = await response.text();
-        document.getElementById("app").innerHTML = content;
+        let container = document.getElementById("app");
+        container.innerHTML = content;
+        container.querySelectorAll("script").forEach(oldScript => {
+            let newScript = document.createElement("script");
+            newScript.textContent = oldScript.textContent;
+            newScript.setAttribute("src", oldScript.getAttribute("src"));
+            newScript.setAttribute("type", oldScript.getAttribute("type"));
+            document.body.appendChild(newScript); 
+        });
     } catch (error) {
         document.getElementById("app").innerHTML = "<h2>Page non trouvée</h2>";
     }
