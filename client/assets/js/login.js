8a86f334b0badcf93fbda2ae5025e350c4e5845f
invader237 <diego.trivino.fr@gmail.com>
2025-03-09 22:40:18 +0100

    :sparkles:(auth): add error handling for login process

--- client/assets/js/login.js
+++ client/assets/js/login.js
@@ -13,20 +13,16 @@ form.addEventListener("submit", (event) => {
 async function authentifier() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    console.log(email, password);
     await login(email, password).then((response) => {
         if (response.status === 200) {
             localStorage.setItem("authToken", response.data.token);
-            alert("Vous êtes connecté");
             window.location.href = "/pages/profil.html";
         }
-        else {
-            document.getElementById("error").innerText = response.data.message
-        }
     }).catch((error) => {
-        console.error("error");
-        console.error(error);
-        document.getElementById("error").innerText = error.data.message;
+        const errorMessage = document.getElementById("error");
+        errorMessage.innerText = error.data.message;
+        errorMessage.attributes.removeNamedItem("hidden");
+
     });
 }
 
