0afc60d1b097791456a6e8a3cf0ec59d85363b6a
Thomas <thom576xd@gmail.com>
2025-03-09 18:47:11 +0100

    :sparkles:(productsDetails): implement logic to retrieve sizes associated to a product

---
+++ serveur/app/DAO/SizeDAO.php
@@ -0,0 +1,33 @@
+<?php
+require_once('./app/core/Connexion.php');
+require_once('./app/entity/Size.php');
+
+class SizeDAO {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getAllSizes(): array {
+        $stmt = $this->pdo->prepare('SELECT * FROM TAILLE');
+        $stmt->execute();
+
+        $sizes = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $sizes[] = new Size($row['id_taille'], $row['libelle']);
+        }
+        
+        return $sizes;
+    }
+
+    public function getSizeById(int $id): ?Size {
+        $stmt = $this->pdo->prepare('SELECT * FROM TAILLE WHERE id_taille = :id');
+        $stmt->execute(['id' => $id]);
+        $row = $stmt->fetch();
+        if ($row === false) {
+            return null;
+        }
+        return new Size($row['id_taille'], $row['libelle']);
+    }
+}
