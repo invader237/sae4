b06e447847073d49cac4786f81f1ae6b1b4bc3a5
Thomas <thom576xd@gmail.com>
2025-03-09 19:23:53 +0100

    :sparkles:(productsDetails): implement logic to retrieve colors associated to a product

--- serveur/app/DAO/ProductDAO.php
+++ serveur/app/DAO/ProductDAO.php
@@ -17,6 +17,7 @@ class ProductDAO {
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $product = new Product($row['id_produit'], $row['designation'], $row['description'], $row['prix'], $row['url_image'], $row['id_categorie']);
             $product->setSizes($this->getSizesByProductId($row['id_produit']));
+            $product->setCouleurs($this->getColorsByProductId($row['id_produit']));
             $products[] = $product;
         }
         
@@ -37,12 +38,18 @@ class ProductDAO {
                       JOIN TAILLE ON TAILLE.id_taille = TAILLE_PRODUIT.id_taille";
         }
 
+<<<<<<< HEAD
         $conditions = [];
         
         if ($search) {
             $conditions[] = "CONCAT(designation, ' ', description, ' ', CATEGORIE.libelle) LIKE :search";
             $params[':search'] = "%$search%";
         }
+=======
+        $product = new Product($row['id_produit'], $row['designation'], $row['description'], $row['prix'], $row['url_image'], $row['id_categorie']);
+        $product->setSizes($this->getSizesByProductId($id));
+        $product->setCouleurs($this->getColorsByProductId($id));
+>>>>>>> 3e58bb7 (:sparkles:(productsDetails): implement logic to retrieve colors associated to a product)
 
         if ($color) {
             $conditions[] = "COULEUR.libelle = :couleur";
