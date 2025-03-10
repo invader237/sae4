c5fd51bc2d26b6039ec230c4a726a349a82f6a03
invader237 <diego.trivino.fr@gmail.com>
2025-03-10 19:21:32 +0100

    :art:(componentsLoader.js): code formatting

--- client/assets/js/componentsLoader.js
+++ client/assets/js/componentsLoader.js
@@ -1,9 +1,7 @@
 function componentsLoader(path, id) {
-        fetch(path)
-                .then(response => response.text())
-                .then(data => document.getElementById(id).innerHTML = data)
-                .catch(error => console.error("Erreur lors du chargement du composant :", error));
+    fetch(path)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error("Erreur lors du chargement du composant :", error));
 }
 
-componentsLoader("./components/header.html", "printHeader");
-componentsLoader("./components/footer.html", "printFooter");
