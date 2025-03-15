233a535b5b311f5ceead19786e171a5c18dabdd2
invader237 <diego.trivino.fr@gmail.com>
2025-03-15 02:39:35 +0100

    :lipstick:(UI): improve responsiveness of product cards

--- client/www/assets/js/accueil.js
+++ client/www/assets/js/accueil.js
@@ -4,23 +4,30 @@ function afficherProduits() {
     getAllProducts().then((response) => {
         const products = response.data;
         const productsContainer = document.getElementById("productsContainer");
+
         products.forEach((product) => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("col-lg-3", "col-md-4", "col-sm-6", "col-12");
+
             const productCard = document.createElement("div");
-            productCard.classList.add("card");
-            productCard.style.width = "18rem";
+            productCard.classList.add("card", "p-3", "shadow-sm", "m-1", "h-100", "d-flex", "flex-column");
             product.url_image = product.url_image.replace(" ", "%20");
+
             productCard.innerHTML = `
-            <img src="https://gitlab.univ-lorraine.fr/laroche5/sae401_2425/-/raw/master/serveur/img/articles/${product.url_image}?ref_type=heads" class="card-img-top w-100 h-100" alt="..." style="object-fit: cover;">
-            <div class="card-body">
-                <h5 class="card-title">${product.designation}</h5>
-                <p class="card-text">${product.prix} €</p>
-                <a href="/pages/produit.html?id=${product.id_produit}" class="btn btn-primary">Voir le produit</a>
-            </div>
+                <img src="https://gitlab.univ-lorraine.fr/laroche5/sae401_2425/-/raw/master/serveur/img/articles/${product.url_image}?ref_type=heads" 
+                    class="card-img-top w-100" 
+                    alt="Image produit" 
+                    style="height: 250px; object-fit: cover;">
+                <div class="card-body flex-grow-1 d-flex flex-column">
+                    <h5 class="card-title">${product.designation}</h5>
+                    <p class="card-text">${product.prix} €</p>
+                    <a href="/pages/produit.html?id=${product.id_produit}" class="btn btn-primary mt-auto">Voir le produit</a>
+                </div>
             `;
-            productCard.classList.add("m-1");
-            productsContainer.appendChild(productCard);
+            productDiv.appendChild(productCard);
+            productsContainer.appendChild(productDiv);
         });
     }).catch((error) => console.error(error));
 }
-afficherProduits();
 
+afficherProduits();
