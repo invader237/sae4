e2e3ae8eb4f52c91faa3a8a93f2e7060e6e22407
Alexandre Kerber <alexkerber54190@gmail.com>
2025-03-15 17:00:03 +0100

    :construction: WIP

--- serveur/app/core/Api.php
+++ serveur/app/core/Api.php
@@ -3,6 +3,7 @@ require_once('./app/controllers/AuthController.php');
 require_once('./app/controllers/ProductController.php');
 require_once('./app/controllers/SizeController.php');
 require_once('./app/controllers/ColorController.php');
+require_once('./app/controllers/PanierController.php')
 
 function setupRoutes($router) {
     $router->add('POST', "/api/auth/login", [AuthController::class, 'login']);
@@ -11,4 +12,5 @@ function setupRoutes($router) {
     $router->add('GET', "/api/getProductByIdAndColorAndSize", [ProductController::class, "getProductByIdAndColorAndSize"]);
     $router->add("GET","/api/getSizesByProductId", [SizeController::class, "getSizesByProductId"]);
     $router->add("GET","/api/getColorsByProductId", [ColorController::class, "getColorsByProductId"]);
+    $router->add('GET',"/api/getPanier", [PanierController::class, "getPanier"]);
 }
