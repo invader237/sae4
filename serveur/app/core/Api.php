396e72480c2fed5d88b650a1f6d89c86d4ab4d52
Thomas <thom576xd@gmail.com>
2025-03-15 23:06:58 +0100

    :bug:(Route): Fixed routing issues

--- serveur/app/core/Api.php
+++ serveur/app/core/Api.php
@@ -8,11 +8,7 @@ function setupRoutes($router) {
     $router->add('POST', "/api/auth/login", [AuthController::class, 'login']);
     $router->add('POST', "/api/auth/register", [AuthController::class, 'register']);
     $router->add('GET', "/api/getAllProducts", [ProductController::class, "getAllProducts"]);
-    $router->add('POST', "/api/getProductById", [ProductController::class, "getProductById"]);
-    $router->add("POST","/api/getSizesByProductId", [ProductController::class, "getSizesByProductId"]);
-    $router->add("POST","/api/getColorsByProductId", [ProductController::class, "getColorsByProductId"]);
-    $router->add("GET","/api/getAllSizes", [SizeController::class, "getAllSizes"]);
-    $router->add("POST","/api/getSizeById", [SizeController::class, "getSizeById"]);
-    $router->add("GET","/api/getAllColors", [ColorController::class, "getAllColors"]);
-    $router->add("POST","/api/getColorById", [ColorController::class, "getColorById"]);
+    $router->add('POST', "/api/getProductByIdAndColorAndSize", [ProductController::class, "getProductByIdAndColorAndSize"]);
+    $router->add("POST","/api/getSizesByProductId", [SizeController::class, "getSizesByProductId"]);
+    $router->add("POST","/api/getColorsByProductId", [ColorController::class, "getColorsByProductId"]);
 }
