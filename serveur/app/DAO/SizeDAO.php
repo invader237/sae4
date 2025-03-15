f232ae7fb0b56f8e791392b9df777cefa1c95708
Thomas <thom576xd@gmail.com>
2025-03-15 23:04:38 +0100

    :bug:(Color and Size): Removed getColorById and getSizeById, and moved getColorByProductId and getSizeByProductId

--- serveur/app/DAO/SizeDAO.php
+++ serveur/app/DAO/SizeDAO.php
@@ -9,25 +9,20 @@ class SizeDAO {
         $this->pdo = $pdo;
     }
 
-    public function getAllSizes(): array {
-        $stmt = $this->pdo->prepare('SELECT * FROM TAILLE');
-        $stmt->execute();
+    public function getSizesByProductId(int $id): array {
+        $stmt = $this->pdo->prepare('
+            SELECT TAILLE.id_taille, TAILLE.libelle 
+            FROM TAILLE 
+            JOIN TAILLE_PRODUIT ON TAILLE.id_taille = TAILLE_PRODUIT.id_taille 
+            WHERE TAILLE_PRODUIT.id_produit = :id
+        ');
+        $stmt->execute(['id' => $id]);
 
         $sizes = [];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $sizes[] = new Size($row['id_taille'], $row['libelle']);
         }
-        
-        return $sizes;
-    }
 
-    public function getSizeById(int $id): ?Size {
-        $stmt = $this->pdo->prepare('SELECT * FROM TAILLE WHERE id_taille = :id');
-        $stmt->execute(['id' => $id]);
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($row === false) {
-            return null;
-        }
-        return new Size($row['id_taille'], $row['libelle']);
+        return $sizes;
     }
 }
