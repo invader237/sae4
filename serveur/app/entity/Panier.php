b7b9eb61c2085f12d33d07aa9d105f7abc58bcf8
invader237 <diego.trivino.fr@gmail.com>
2025-03-17 23:36:42 +0100

    wip

--- serveur/app/entity/Panier.php
+++ serveur/app/entity/Panier.php
@@ -6,27 +6,21 @@ class Panier {
     private $id_utilisateur;
     private array $produits = []; 
 
-<<<<<<< HEAD
     public function __construct($id_panier, $id_utilisateur, $produits = []) {
         $this->id_panier = $id_panier;
         $this->id_utilisateur = $id_utilisateur;
         $this->produits = $produits; 
-=======
-    public function __construct($id_panier,$id_utilisateur,$id_produit,$qte,$id_taille,$id_couleur){
-        $this->id_panier=$id_panier;
-        $this->id_utilisateur=$id_utilisateur;
-        $this->id_produit=$id_produit;
-        $this->qte=$qte;
-        $this->id_taille=$id_taille;
-        $this->id_couleur=$id_couleur;
->>>>>>> eeb2b40 (wip)
     }
 
     public function getIdPanier(): int {
         return $this->id_panier;
     }
 
-    public function getIdUtilisateur(): int {
+    public function getIdPanier() {
+        return $this->id_panier;
+    }
+
+    public function getIdUtilisateur() {
         return $this->id_utilisateur;
     }
 
