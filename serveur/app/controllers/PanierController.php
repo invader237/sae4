337131ecce0764331b965275de4466c4538ca78d
invader237 <diego.trivino.fr@gmail.com>
2025-03-18 22:51:25 +0100

    :sparkles:(shopCart): add user cart recovery

--- serveur/app/controllers/PanierController.php
+++ serveur/app/controllers/PanierController.php
@@ -8,78 +8,7 @@ class PanierController {
         
         $id_utilisateur=AuthMiddleware::getUser();
         $panier = PanierService::getPanier($id_utilisateur);
-        echo json_encode(["data" => $panier.toArray()], JSON_UNESCAPED_UNICODE);
+        echo json_encode(["data" => $panier->toArray()], JSON_UNESCAPED_UNICODE);
     }
 
-    /*
-    public function addProduitPanier() {
-        header('Content-Type: application/json');
-        
-        $id_utilisateur=AuthMiddleware::getUser();
-        
-        $body = file_get_contents('php://input');
-        $data = json_decode($body, true);
-
-        if (!isset($data['id_produit'], $data['qte'], $data['id_taille'], $data['id_couleur'])) {
-            http_response_code(400);
-            echo json_encode(["message" => "Tous les champs sont requis"]);
-            return;
-        }
-        
-        $success = PanierService::addProduitPanier(
-            $id_utilisateur, 
-            $data['id_produit'], 
-            $data['qte'], 
-            $data['id_taille'], 
-            $data['id_couleur']
-        );
-        echo json_encode(["message" => $success ? "Produit ajouté" : "Erreur lors de l'ajout"]);    
-    }
-    
-
-    public function updateQuantiteProduit() {
-        header('Content-Type: application/json');
-        
-        $id_utilisateur=AuthMiddleware::getUser();
-
-        $body = file_get_contents('php://input');
-        $data = json_decode($body, true);
-        
-        if (!isset($data['id_produit'], $data['qte'], $data['id_taille'], $data['id_couleur'])) {
-            http_response_code(400);
-            echo json_encode(["message" => "Tous les champs sont requis"]);
-            return;
-        }
-        
-        $success =PanierService::updateQuantiteProduit($id_utilisateur, $data['id_produit'], $data['qte'], $data['id_taille'], $data['id_couleur']);
-        echo json_encode(["message" => $success ? "Quantité mise à jour" : "Erreur lors de la mise à jour"]);
-    }
-
-    public function deleteProduitPanier() {
-        header('Content-Type: application/json');
-        
-        $id_utilisateur=AuthMiddleware::getUser();
-
-        $body = file_get_contents('php://input');
-        $data = json_decode($body, true);
-        
-        if (!isset($data['id_produit'], $data['id_taille'], $data['id_couleur'])) {
-            http_response_code(400);
-            echo json_encode(["message" => "Tous les champs sont requis"]);
-            return;
-        }
-        
-        $success = PanierService::deleteProduitPanier($id_utilisateur, $data['id_produit'], $data['id_taille'], $data['id_couleur']);
-        echo json_encode(["message" => $success ? "Produit supprimé" : "Erreur lors de la suppression"]);
-    }
-
-    public function clearPanier() {
-        header('Content-Type: application/json');
-
-        $id_utilisateur=AuthMiddleware::getUser();
-        
-        $success = PanierSerivce::clearPanier($id_utilisateur);
-        echo json_encode(["message" => $success ? "Panier vidé" : "Erreur lors du vidage du panier"]);
-    }
-    */
 }
