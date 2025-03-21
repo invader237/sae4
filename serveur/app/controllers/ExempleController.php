67a50ecb8fd0a1c8a0cecc9f30cf794dc1b853c1
invader237 <diego.trivino.fr@gmail.com>
2025-03-21 01:19:59 +0100

    :fire:(core): remove unnecessary test controller

--- serveur/app/controllers/ExempleController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ExempleController {
-    public static function index() {
-        header('Content-Type: application/json');
-        echo json_encode(["message" => "Exemple de endpoint"]);
-    }
-}
--- serveur/app/core/Api.php
+++ serveur/app/core/Api.php
@@ -6,7 +6,6 @@ require_once('./app/controllers/SizeController.php');
 require_once('./app/controllers/ColorController.php');
 
 function setupRoutes($router) {
-    $router->add('GET', "/api/home", [ExempleController::class, 'index']);
     $router->add('POST', "/api/auth/login", [AuthController::class, 'login']);
     $router->add('POST', "/api/auth/register", [AuthController::class, 'register']);
     $router->add('GET', "/api/getAllProducts", [ProductController::class, "getAllProducts"]);
