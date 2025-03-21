09fc0c27fb99b05e56058c267b9d09dc62ab0438
invader237 <diego.trivino.fr@gmail.com>
2025-03-21 01:19:59 +0100

    :fire:(core): remove unnecessary test controller

--- serveur/app/core/Api.php
+++ serveur/app/core/Api.php
@@ -4,7 +4,6 @@ require_once('./app/controllers/AuthController.php');
 require_once('./app/controllers/ProductController.php');
 
 function setupRoutes($router) {
-    $router->add('GET', "/api/home", [ExempleController::class, 'index']);
     $router->add('POST', "/api/auth/login", [AuthController::class, 'login']);
     $router->add('POST', "/api/auth/register", [AuthController::class, 'register']);
     $router->add('GET', "/api/getAllProducts", [ProductController::class, "getAllProducts"]);
