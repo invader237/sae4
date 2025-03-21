35e413cdd6a3a615b061c129e1d516fcedada1c8
invader237 <diego.trivino.fr@gmail.com>
2025-03-21 01:15:49 +0100

    :fire:(api.js): remove outdated comments

--- client/www/assets/js/core/api/api.js
+++ client/www/assets/js/core/api/api.js
@@ -1,16 +1,5 @@
 import axiosInstance from "./axiosConfig.js";
 
-/* 
-export const <functionName> = async () => {
-    try {
-        const response = await axiosInstance.get("<apiEndpoint>");
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-    };
-*/
-
 export const login = async (email, password) => {
     try {
         const response = await axiosInstance.post("/auth/login", {
