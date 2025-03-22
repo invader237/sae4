d7904b94b288691bac2460ef3955f8d8ea4b4599
invader237 <diego.trivino.fr@gmail.com>
2025-03-22 14:55:20 +0100

    :lock:(security): update allowed methods to prevent verb tampering attacks

--- serveur/index.php
+++ serveur/index.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
     header('Access-Control-Allow-Origin:*');
