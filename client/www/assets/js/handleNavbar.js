b9549d95e5a6705bfda29cd4f8760f076f2706c9
Thomas <thom576xd@gmail.com>
2025-03-25 00:32:26 +0100

    sparkles:(connectionStatus): Implemented display of the user icon only is the user is connected

--- client/www/assets/js/handleNavbar.js
+++ client/www/assets/js/handleNavbar.js
@@ -1,19 +1,31 @@
+let connected;
+
 function handleNavbar(size) { // size -> Taille en px à partir de laquelle le menu se cache 
     document.addEventListener("DOMContentLoaded", function() {
-        let screenSize = window.matchMedia('(max-width: ' + size + 'px)');
+        waitForElement(".navbar-nav", () => {
+            let screenSize = window.matchMedia('(max-width: ' + size + 'px)');
 
-        screenSize.addEventListener("change", toggleNavbar);
-        toggleNavbar(screenSize)
-
-        // Quand on appuie sur le bouton -> Lance la fonction
-        document.querySelector(".navbar-toggler").addEventListener("click", function() {
-            let nav = document.querySelector(".navbar-nav");
-
-            if (nav.style.display === "block") { // Si il est en bloc, cela veut dire qu'il est actuellement affiché
-                nav.style.display = "none"; // On le cache 
+            if (localStorage.getItem("authToken")) {
+                connected = true;
             } else {
-                nav.style.display = "block"; // Sinon on le définit en tant que block (Le ul n'est plus caché)
+                connected = false;
+                document.getElementById("navUserIcon").style.display = "none";
+                document.getElementById("userIcon").style.display = "none";
             }
+
+            screenSize.addEventListener("change", toggleNavbar);
+            toggleNavbar(screenSize)
+
+            // Quand on appuie sur le bouton -> Lance la fonction
+            document.querySelector(".navbar-toggler").addEventListener("click", function() {
+                let nav = document.querySelector(".navbar-nav");
+
+                if (nav.style.display === "block") { // Si il est en bloc, cela veut dire qu'il est actuellement affiché
+                    nav.style.display = "none"; // On le cache 
+                } else {
+                    nav.style.display = "block"; // Sinon on le définit en tant que block (Le ul n'est plus caché)
+                }
+            });
         });
     });
 }
@@ -24,7 +36,9 @@ function toggleNavbar(element) {
         document.querySelector(".navbar-nav").style.display = "none";
         document.querySelector(".navbar-toggler").style.display = "block";
         document.getElementById("navbarNav").classList.remove("me-3");
-        document.getElementById("userIcon").style.display = "block";
+        if (connected) {
+            document.getElementById("userIcon").style.display = "block";
+        }
         document.getElementById("navUserIcon").style.display = "none";
     } else {
         // Sinon cache le bouton, affiche le menu de navigation
@@ -32,6 +46,25 @@ function toggleNavbar(element) {
         document.querySelector(".navbar-toggler").style.display = "none";
         document.getElementById("navbarNav").classList.add("me-3");
         document.getElementById("userIcon").style.display = "none";
-        document.getElementById("navUserIcon").style.display = "block";
+        if (connected) {
+            document.getElementById("navUserIcon").style.display = "block";
+        }
+    }
+}
+
+function waitForElement(selector, callback) {
+    const element = document.querySelector(selector);
+    if (element) {
+        callback(element);
+    } else {
+        const observer = new MutationObserver(() => {
+            const elem = document.querySelector(selector);
+            if (elem) {
+                callback(elem);
+                observer.disconnect(); // Stopper l'observation après avoir trouvé l'élément
+            }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
     }
 }
