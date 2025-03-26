dcdd076932a6d37b50cea7b1a8b5b51e5cf9de49
Thomas <thom576xd@gmail.com>
2025-03-26 17:45:13 +0100

    :sparkles:(profile): Added a profile page

--- client/www/assets/js/profil.js
+++ client/www/assets/js/profil.js
@@ -5,3 +5,10 @@ const response = await getUser();
 if (response === undefined) {
     window.location.href = "./login.html";
 }
+
+const user = response.data
+document.getElementById("name").innerHTML = user.name;
+document.getElementById("firstName").innerHTML = user.firstName;
+document.getElementById("civ").innerHTML = user.idTitle;
+document.getElementById("mail").innerHTML = user.email;
+document.getElementById("birthDate").innerHTML = user.birthDate;
